546a51a4a2e7dd5a9caf765ce20a5b7ac17e2614
Iain George <theiguana@gmail.com>
2020-01-25 16:22:55 -0700

    Initial Commit

---
+++ MemeMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by Iain George on 2020-01-25.
+//  Copyright © 2020 Iain George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
