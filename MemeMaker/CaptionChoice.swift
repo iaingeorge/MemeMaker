875a833d15001878202e3c8f12d6bcee53b84b6c
Iain George <theiguana@gmail.com>
2020-01-25 17:01:53 -0700

    Setup the type for captions via a struc

---
+++ MemeMaker/CaptionChoice.swift
@@ -0,0 +1,14 @@
+//
+//  CaptionChoice.swift
+//  MemeMaker
+//
+//  Created by Iain George on 2020-01-25.
+//  Copyright © 2020 Iain George. All rights reserved.
+//
+
+import Foundation
+
+struct CaptionOption {
+    let caption: String
+    let emoji: String
+}
